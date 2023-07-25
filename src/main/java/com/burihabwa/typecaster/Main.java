797e9588cfcb705be286d568c084f0eebc6b69c5
Dorian Burihabwa <dorian@burihabwa.com>
2023-07-25 11:50:10 +0200

    Read configuration from path on the command line

--- src/main/java/com/burihabwa/typecaster/Main.java
+++ src/main/java/com/burihabwa/typecaster/Main.java
@@ -1,11 +1,25 @@
 package com.burihabwa.typecaster;
 
+import org.apache.commons.cli.*;
+
+import java.nio.file.Path;
 import java.util.logging.Logger;
 
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getCanonicalName());
 
-    public static void main(String[] args) {
-        logger.info("Hello, World!");
+    public static void main(String[] args) throws ParseException {
+        Options options = new Options();
+        options.addRequiredOption("p", "path", true, "Path to the project to analyze");
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+        if (!cmd.hasOption("p")) {
+            logger.warning("path argument is missing!");
+            System.exit(0);
+        }
+        var path = Path.of(cmd.getOptionValue("path"));
+        Configuration configuration = ConfigurationBuilder.newInstance()
+                .setPath(path)
+                .build();
     }
 }
