2620922de0c311265cfeb409fb2193c37455b6c2
Dorian Burihabwa <dorian@burihabwa.com>
2025-02-22 18:36:38 +0100

    Use SLF4J for logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,15 @@ repositories {
 }
 
 dependencies {
+    implementation("ch.qos.logback:logback-classic:1.5.16")
     implementation("commons-cli:commons-cli:1.5.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    implementation("org.slf4j:slf4j-api:2.0.16")
+
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+
     testImplementation("org.assertj:assertj-core:3.23.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
 }
 repositories {
     mavenCentral()
--- src/main/java/com/burihabwa/typecaster/Main.java
+++ src/main/java/com/burihabwa/typecaster/Main.java
@@ -2,11 +2,13 @@ package com.burihabwa.typecaster;
 
 import org.apache.commons.cli.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.nio.file.Path;
-import java.util.logging.Logger;
 
 public class Main {
-    private static final Logger logger = Logger.getLogger(Main.class.getCanonicalName());
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws ParseException {
         Options options = new Options();
@@ -14,12 +16,14 @@ public class Main {
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, args);
         if (!cmd.hasOption("p")) {
-            logger.warning("path argument is missing!");
+            logger.warn("path argument is missing!");
             System.exit(0);
         }
         var path = Path.of(cmd.getOptionValue("path"));
+        logger.atInfo().log("Analyzing project: %s".formatted(path.toAbsolutePath()));
         Configuration configuration = ConfigurationBuilder.newInstance()
                 .setPath(path)
                 .build();
+        logger.atInfo().log(configuration.toString());
     }
 }
